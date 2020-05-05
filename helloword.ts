4f8becc3e2a97910c3612e20ba758e668f0d7844
wdmlsx <wdmlsx@163.com>
2020-05-05 10:33:35 +0800

    init

---
+++ helloword.ts
@@ -0,0 +1,22 @@
+export interface Hello {
+    name: string;
+    age: number;
+    address: string;
+}
+export class HelloWord {
+    // public name: string;
+    // public age: number;
+    // public address: string;
+    // ADDRESS
+    
+    constructor(private readonly info: Hello) {}
+}
+
+const p = new HelloWord({
+    name: 'wangdm',
+    age: 27,
+    address: 'jiangsu',
+});
+// console.log()
+
+console.log('p: ', JSON.stringify(p, null, 2));
